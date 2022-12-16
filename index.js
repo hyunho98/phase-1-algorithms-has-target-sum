6910e8cccce16178fcd289875d2c8c92708201ba
Alex Lee <hyunho98@gmail.com>
2022-12-16 07:21:21 -0500

    commit tree 1e4f548c4e98c5fa3298f9d0fb6212ea2298094e

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let n = i + 1; n < array.length; n++) {
+      if (array[i] + array[n] === target)
+        return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  iterate over each element of the array
+    iterate after each element of the array that comes after the current element
+      return true if the target is hit
+  return false since the target was not hit
 */
 
 /*
   Add written explanation of your solution here
+  To find all possible sums, each number in the array must be
+  matched with every other number in the array making the best possible
+  Big 0 time complexity 0(n^2)
 */
 
 // You can run `node index.js` to view these console logs
